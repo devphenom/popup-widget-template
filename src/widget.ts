618f30dbb74d54a491b6bbd77f11972d5043e6ec
devpenom <iphenom01@gmail.com>
2022-05-21 02:01:51 +0100

    Initialize

--- src/widget.ts
+++ src/widget.ts
@@ -1,26 +1,10 @@
-const defaultStyles: any = {
-  'border': 'none',
-  'z-index': 2147483647,
-  'height': '650px',
-  'width': '350px',
-  'display': 'block !important',
-  'visibility': 'visible',
-  'background': 'none transparent',
-  'opacity': 1,
-  'pointer-events': 'auto',
-  'touch-action': 'auto',
-  'position': 'fixed',
-  'right': '20px',
-  'bottom': '20px',
-}
-
 interface IConfig {
   readonly email: string;
 }
 
 interface IWidget {
   config: IConfig | null;
-  iframe: HTMLIFrameElement | null;
+  iframe: Window | null;
   init: (config: IConfig) => void;
   setupListeners: () => void;
   createIframe: () => void;
@@ -30,38 +14,34 @@ interface IWidget {
 const Widget: IWidget = {
   iframe: null,
   config: null,
-  init: function(config: IConfig) {
+  init: function (config: IConfig) {
     this.config = config;
-    this.createIframe()
+    console.log(config);
+    this.createIframe();
   },
-  createIframe: function() {
-    this.iframe = document.createElement('iframe');
-    let styles = '';
-    for (let key in defaultStyles) { styles += key + ': ' + defaultStyles[key] + ';' }
-    this.iframe.setAttribute('style', styles)
-    this.iframe.src = 'http://localhost:9000';
-    this.iframe.referrerPolicy = 'origin';
-    document.body.appendChild(this.iframe);
+  createIframe: function () {
+    this.iframe = window.open("http://localhost:9000", "null", "top:0");
     this.setupListeners();
   },
-  setupListeners: function() {
-    window.addEventListener('message', this.handleMessage.bind(this));
+  setupListeners: function () {
+    window.addEventListener("message", this.handleMessage.bind(this));
   },
-  handleMessage: function(e) {
+  handleMessage: function (e) {
+    console.log("data", e);
     e.preventDefault();
-    if (!e.data || (typeof e.data !== 'string')) return;
+    if (!e.data || typeof e.data !== "string") return;
     let data = JSON.parse(e.data);
     switch (data.action) {
-      case 'init': {
+      case "init": {
         if (this.iframe) {
-          this.iframe.contentWindow.postMessage(JSON.stringify(this.config), '*');
+          this.iframe.postMessage(JSON.stringify(this.config), "*");
         }
         break;
       }
       default:
         break;
     }
-  }
+  },
 };
 
 export default Widget;
